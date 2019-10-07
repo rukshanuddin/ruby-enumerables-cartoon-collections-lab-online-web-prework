ecaedfd6050a7d01888dbf7875a9cbea44d94a33
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-07 13:32:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_call(planeteer_calls)
-  planeteer_calls.keep_if {|a| a.length < 5}
+  return planeteer_calls.keep_if {|a| a.length < 5}
 end
 
 def find_the_cheese# code an argument here
