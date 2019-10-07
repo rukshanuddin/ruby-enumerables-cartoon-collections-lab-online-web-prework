9abec7e4bda9b4c79b8e50d36f378e204a1cd924
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-07 13:41:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_call(planeteer_calls)
-  planeteer_calls.count {|a| a.length > 4}
+  return planeteer_calls.count {|a| a.length > 4}
 
 end
 
